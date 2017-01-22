d55d112984c099bc88b40bf0031ee891565a6809
claudiordgz <claudio.rdgz@gmail.com>
2017-01-22 02:03:12 -0500

    notes: finished validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,6 +12,7 @@
 
 import pickle
 import sys
+from time import time
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -28,5 +29,22 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+t0 = time()
+clf.fit(features_train, labels_train) 
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "pred time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print "accuracy ", acc, "s"
 
